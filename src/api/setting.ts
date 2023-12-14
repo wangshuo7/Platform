c39d470c5fe1f9b3b22363eda63c3c801142cbae
wangshuo7 <13665439520@163.com>
2023-12-14 11:14:02 +0800

    fix：修复首次进入bug

--- src/api/setting.ts
+++ src/api/setting.ts
@@ -2,7 +2,7 @@ import request from '../utils/request'
 import { settingList } from '../type/setting'
 
 // 获取系统配置
-export const getConfig = (data: settingList) => {
+export const getConfig = (data: any) => {
   return request.post('/pingtai/setconf/getconf', data)
 }
 // 设置系统配置
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -9,6 +9,15 @@ export default {
   name: 'Home'
 }
 </script>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import { getConfig } from '../../api/setting'
+async function query() {
+  await getConfig({})
+}
+onMounted(() => {
+  query()
+})
+</script>
 
 <style lang="less" scoped></style>
