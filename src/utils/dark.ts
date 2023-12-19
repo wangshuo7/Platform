27c5560f3e9785de91877847e27baa02396feaf0
wangshuo7 <13665439520@163.com>
2023-12-19 11:13:39 +0800

    feat：修改暗黑模式保存到sessionStorage中

--- src/utils/dark.ts
+++ src/utils/dark.ts
@@ -1,4 +1,6 @@
 import { useDark, useToggle } from '@vueuse/core'
 
-export const isDark = useDark()
+export const isDark = useDark({
+  storage: sessionStorage
+})
 export const toggleDark = useToggle(isDark)
